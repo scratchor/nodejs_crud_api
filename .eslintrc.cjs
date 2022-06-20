f3d468dcceeb182110534fb8bc48f6e4672aab5c
utsarapkin <utsarapkin@fnc-group.ru>
2022-06-20 23:43:12 +0300

    fix: eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,21 +17,4 @@ module.exports = {
         '@typescript-eslint/no-unused-vars': 'warn',
         "no-restricted-syntax": ["error", "ForInStatement", "LabeledStatement", "WithStatement"]
     },
-    // overrides: [
-    //     {
-    //         files: ['*.ts', '*.tsx'], // Your TypeScript files extension
-    //
-    //         // As mentioned in the comments, you should extend TypeScript plugins here,
-    //         // instead of extending them outside the `overrides`.
-    //         // If you don't want to extend any rules, you don't need an `extends` attribute.
-    //         extends: [
-    //             'plugin:@typescript-eslint/recommended',
-    //             'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    //         ],
-    //
-    //         parserOptions: {
-    //             project: ['./tsconfig.json'], // Specify it only for TypeScript files
-    //         },
-    //     },
-    // ],
 };
